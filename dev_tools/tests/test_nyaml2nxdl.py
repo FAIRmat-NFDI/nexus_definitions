1686eeb145c43584e30f1c80c9efbe6cb54e1735
Rubel <rubel.mozumder@outlook.com>
2023-10-31 09:38:44 +0100

    Fix git workflow action.

--- dev_tools/tests/test_nyaml2nxdl.py
+++ dev_tools/tests/test_nyaml2nxdl.py
@@ -13,8 +13,7 @@ def test_conversion():
     result = CliRunner().invoke(conv.launch_tool, ["--input-file", str(root)])
     assert result.exit_code == 0
     # Replace suffixes
-    yaml = root.with_suffix("").with_suffix(".yaml")  # replace .nxdl.xml
-    yaml = yaml.with_stem(yaml.stem + "_parsed")  # extend file name with _parsed
+    yaml = root.with_suffix("").with_suffix("_parsed.yaml")  # replace .nxdl.xml
     result = CliRunner().invoke(conv.launch_tool, ["--input-file", str(yaml)])
     assert result.exit_code == 0
     new_root = yaml.with_suffix(".nxdl.xml")  # replace yaml
