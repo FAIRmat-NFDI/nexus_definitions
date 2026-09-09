b4d0846cc8d5cbea19551524b43d88d957d5cfaf
mkuehbach <mkuehbach@users.noreply.github.com>
2026-09-09 14:36:30 +0000

    deploy: af199a51d2c28ea162e7da411c8425519573ef53

---
+++ docs/_downloads/2d54d9af02ee715da23fed8d858526ba/simple_example_write1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+Writes the simplest NeXus HDF5 file using h5py
+
+Uses method accepted at 2014NIAC
+according to the example from Figure 1.3
+in the Introduction chapter
+"""
+
+from pathlib import Path
+
+import numpy
+from nexusformat.nexus import NXdata
+from nexusformat.nexus import NXentry
+from nexusformat.nexus import NXfield
+from nexusformat.nexus import nxopen
+
+filename = str(Path(__file__).absolute().parent.parent / "simple_example.dat")
+buffer = numpy.loadtxt(filename).T
+tthData = buffer[0]
+countsData = numpy.asarray(buffer[1], "int32")
+
+with nxopen("simple_example_write1.hdf5", "w") as f:  # create the NeXus file
+    f["Scan"] = NXentry()
+    tth = NXfield(tthData, name="two_theta", units="degrees")
+    counts = NXfield(countsData, name="counts", units="counts")
+    f["Scan/data"] = NXdata(counts, tth)
